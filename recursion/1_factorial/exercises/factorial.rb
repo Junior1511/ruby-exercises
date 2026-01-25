0628f61b0d69aec758b3485afec3f1ec936f2033
Junior1511 <yunior122000@gmail.com>
2026-01-25 12:20:17 -0600

    I am really struggling to understand recursion, but supposed this had to happen at a point or another lol

--- recursion/1_factorial/exercises/factorial.rb
+++ recursion/1_factorial/exercises/factorial.rb
@@ -1,3 +1,8 @@
 def factorial(num)
   # write a recursive method for calculating the [factorial](https://simple.wikipedia.org/wiki/Factorial) of a number
+  
+  # Base case
+  return nil if num < 0
+  return 1 if num <= 1
+  num * factorial(num - 1)
 end

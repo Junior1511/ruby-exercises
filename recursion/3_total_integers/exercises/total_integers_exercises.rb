b90b1b78726a1bb7dbb824efa44113d86bede7ae
Junior1511 <yunior122000@gmail.com>
2026-01-25 15:22:19 -0600

    Just keep going man

--- recursion/3_total_integers/exercises/total_integers_exercises.rb
+++ recursion/3_total_integers/exercises/total_integers_exercises.rb
@@ -7,4 +7,26 @@ def total_integers(array)
   # NOTE: you may notice that `Array#flatten` would make quick work of this,
   # but you should implement this method without using it. The tests will check
   # to make sure `#flatten` isn't used.
+  # 
+  
+  
+  def arr_flatten(array)
+    flatten_num_array = []
+
+    if array.is_a?(Integer)
+      flatten_num_array << array
+    elsif array.is_a?(Array)
+      array.each do |thing_in_array|
+        flatten_num_array += arr_flatten(thing_in_array)
+      end
+    end
+
+    flatten_num_array
+  end
+
+  arr_flatten(array).count
+
+
 end
+
+p total_integers([1,2,3,4,[[[[[[[[[7]]]]]]]]]])
